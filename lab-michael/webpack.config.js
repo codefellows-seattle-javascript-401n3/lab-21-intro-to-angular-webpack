3e78c258f0b7a2412e1efe63ac7d4bb8b4be5e80
Michael Bishop <polishthebishop@gmail.com>
2017-02-02 03:10:30 -0800

    code is building out

--- lab-michael/webpack.config.js
+++ lab-michael/webpack.config.js
@@ -1,2 +1,24 @@
+'use strict';
 
-module.exports = {};
+const HTMLPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: `${__dirname}/app/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: 'build',
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: `${__dirname}/app/index.html`,
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        use:['style-loader', 'css-loader', 'sass-loader']
+      },
+    ],
+  },
+};
